7f444127caace25cc9186cd7c148ca8e57f487e2
Smirnov Ivan <smirnowwwivan@mail.ru>
2023-12-26 23:20:16 +0300

    Update portals

--- Tankport/Assets/Scripts/PortalPlacement.cs
+++ Tankport/Assets/Scripts/PortalPlacement.cs
@@ -40,7 +40,7 @@ public class PortalPlacement : MonoBehaviour
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
 
-        if (Physics.Raycast(ray, out hit, 300f) && !clickMask.Equals(hit.collider.gameObject.layer))
+        if (Physics.Raycast(ray, out hit, 300f) && hit.collider != null)
         {
             clickPosition = hit.point;
         }
